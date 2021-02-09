3d9a700c54ca3db634bdd53e737c72c84cef44b5
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-02-09 21:04:46 +0300

    Update ruler position to match recommendation

--- src/Banana/TypeCheck/Checker.hs
+++ src/Banana/TypeCheck/Checker.hs
@@ -2,7 +2,8 @@
 Module      : Checker
 Description : Semantic Analyzer
 
-Given the parsed AST, the Semantic Checker verifies that the input program is valid
-  and transforms the AST into another intermediate representation for code generation.
+Given the parsed AST, the Semantic Checker verifies that the input program
+  is valid and transforms the AST into another intermediate representation
+  for code generation.
 -}
 module Banana.TypeCheck.Checker where
