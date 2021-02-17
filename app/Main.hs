343e80b36fbfed54216a67d4e366f9235ef40ecf
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-02-17 17:14:25 +0300

    Add parsing expression to main function
    as an example

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import           Banana.Syntax.Parser
+import           Text.Trifecta        (parseString)
+
 main :: IO ()
-main = print "Hello World!"
+main = do
+  line <- getLine
+  case line of
+    ""  -> return ()
+    str -> do
+      print $ parseString parseExpr mempty (str ++ "\n")
+      main
