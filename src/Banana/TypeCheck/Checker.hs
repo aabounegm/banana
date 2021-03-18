4ac0f085196aa539c28a19aea3a28b4cc5892a80
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-03-18 12:47:30 +0300

    Fix check to cover statments, not only assignments

--- src/Banana/TypeCheck/Checker.hs
+++ src/Banana/TypeCheck/Checker.hs
@@ -29,37 +29,42 @@ varToReference _          = const Nothing
 assignmentsAreValid :: Program a -> Bool
 assignmentsAreValid = all (validLHS . varAssignee) . assignments
 
+-- | Checks that no 2 variables are declared with the same name
 varDeclsAreUnique :: (Eq a) => Program a -> Bool
 varDeclsAreUnique p = length decls == length (nub decls)
   where
     decls = map varName $ varDecls p
 
+-- | Checks that all variables used in expressions are declared beforehand
 allVarsDeclared :: (Eq a) => Program a -> Bool
 allVarsDeclared prog = all
   (isJust . (`varToReference` varsTable))
-  (concatMap expressions (assignments prog))
+  (concatMap expressions (statements prog))
   where
-    vars (Add l r) = vars l ++ vars r
-    vars (Sub l r) = vars l ++ vars r
-    vars (Mul l r) = vars l ++ vars r
-    vars (Div l r) = vars l ++ vars r
-    vars (Var a)   = [Var a]
-    vars (Lit _)   = []
-    vars (Not e)   = vars e
-    vars (Or  l r) = vars l ++ vars r
-    vars (And l r) = vars l ++ vars r
-    vars (Eq  l r) = vars l ++ vars r
-    vars (NEq l r) = vars l ++ vars r
-    vars (LEq l r) = vars l ++ vars r
-    vars (MEq l r) = vars l ++ vars r
-    vars (Less l r) = vars l ++ vars r
-    vars (More l r) = vars l ++ vars r
-    vars (FuncCallExpr (FuncCall _ args)) = concatMap vars args
+    vars (Add l r)                       = vars l ++ vars r
+    vars (Sub l r)                       = vars l ++ vars r
+    vars (Mul l r)                       = vars l ++ vars r
+    vars (Div l r)                       = vars l ++ vars r
+    vars (Var a)                         = [Var a]
+    vars (Lit _)                         = []
+    vars (Not e)                         = vars e
+    vars (Or  l r)                       = vars l ++ vars r
+    vars (And l r)                       = vars l ++ vars r
+    vars (Eq  l r)                       = vars l ++ vars r
+    vars (NEq l r)                       = vars l ++ vars r
+    vars (LEq l r)                       = vars l ++ vars r
+    vars (MEq l r)                       = vars l ++ vars r
+    vars (Less l r)                      = vars l ++ vars r
+    vars (More l r)                      = vars l ++ vars r
+    vars (FuncCallExpr (FuncCall _ arg)) = concatMap vars arg
 
-    expressions (VarAssign lhs rhs) = vars lhs ++ vars rhs
+    expressions (VarAssignment (VarAssign lhs rhs))  = vars lhs ++ vars rhs
+    expressions (FuncCallStatement (FuncCall _ arg)) = concatMap vars arg
+    expressions _                                    = []
 
     varsTable = varDecls prog
 
+-- | Runs all the checkers, combining their output
 checkAll :: (Eq a) => Program a -> Bool
 checkAll = and . sequence
   [ allVarsDeclared
